f7b05809e92282af8f1fb144cf84cdc7ed5bbb0f
Razboy20 <razboy20@gmail.com>
2021-10-17 19:39:34 -0500

    works

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@
 # echo "Starting PHP-FPM..."
 # /usr/sbin/php-fpm7 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
-echo "Starting Caddy..."
-./caddy-server run --watch --config ./caddy/Caddyfile &
-
-cd ./ghost && ghost run
+echo "Starting Ghost..."
+cd ./ghost && ghost start &
+./caddy-server run --watch --config ./caddy/Caddyfile
